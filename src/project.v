1f1c5a7a8bc676c75dfdc2759bc1aabccfdb5800
akaur014 <ia0111@wayne.edu>
2026-01-18 18:40:01 -0500

    Update tt_um_akaur014_simple_circuit.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_akaur_simple_circuit (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,35 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    // Map input wires for clarity
+  wire A = ui_in[0];
+  wire B = ui_in[1];
+  wire C = ui_in[2];
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  // Internal wire
+  wire e;
+  wire x;
+  wire y;
+
+  // Logic implementation
+  and g1(e, A, B);
+  not g2(y, C);
+  or  g3(x, e, y);
+
+  // Assign outputs
+  assign uo_out[0] = x;
+  assign uo_out[1] = y;
+  assign uo_out[2] = 1'b0;
+  assign uo_out[3] = 1'b0;
+  assign uo_out[4] = 1'b0;
+  assign uo_out[5] = 1'b0;
+  assign uo_out[6] = 1'b0;
+  assign uo_out[7] = 1'b0;
+
+  assign uio_out = 8'b00000000;
+  assign uio_oe  = 8'b00000000;
+
+  // Prevent unused input warnings
+  wire _unused = &{ena, clk, rst_n, ui_in[7:3], uio_in};
 
 endmodule
